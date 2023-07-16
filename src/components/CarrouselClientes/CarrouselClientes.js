31a28fc9cb278a6d39255d21e0716f0d3c587765
franco-buccellato <francbuccellato@frba.utn.edu.ar>
2023-07-16 20:07:22 -0300

    Agrego logo Fasten y Asecom

--- src/components/CarrouselClientes/CarrouselClientes.js
+++ src/components/CarrouselClientes/CarrouselClientes.js
@@ -2,7 +2,7 @@ import './CarrouselClientes.css';
 import cliente1 from '../../imagenes/Logo-Bimbo.png'
 import cliente2 from '../../imagenes/Logo-Aires.svg'
 import cliente3 from '../../imagenes/Logo-Asecon.png'
-import cliente4 from '../../imagenes/Logo-Fasten.jpg'
+import cliente4 from '../../imagenes/Logo-Fasten.png'
 import cliente5 from '../../imagenes/Logo-GrupoHydralpng.png'
 import cliente6 from '../../imagenes/Logo-Machvision.png'
 import cliente7 from '../../imagenes/Logo-Powercom.png'
